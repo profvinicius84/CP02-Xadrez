462c546909a07ef7904cbf449c8dab1d18ce0c88
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2025-04-25 08:33:30 -0300

    changes

---
+++ Xadrez/Models/Rei.cs
@@ -0,0 +1,21 @@
+namespace Xadrez.Models;
+using Xadrez.Models.Pecas;
+public class Rei : IRei
+{
+
+    public Rei()
+    {
+
+    }
+
+    public bool VerificaRoque(Tabuleiro tabuleiro, bool roquePequeno = false) 
+    {
+
+    }
+
+    public Movimento ExecutaRoque(Tabuleiro tabuleiro, bool roquePequeno = false) 
+    {
+        
+    }
+
+}
